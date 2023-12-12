18c66bd142babcfbc038f8a9c0d6c6c4b5545def
Ali Alafandy <107328467+AliAlafandy@users.noreply.github.com>
2023-12-12 10:24:26 +0200

    Add files via upload

--- build-crash-handler-android.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-cd crashHandler
-echo Building crash dialog...
-haxelib run lime build android
-copy build\openfl\android\bin\app\build\apk\debug\SBCrashHandler.apk ..\export\release\android\bin\app\build\outputs\apk\debug\SBCrashHandler.apk
-cd ..
-
-@echo on
