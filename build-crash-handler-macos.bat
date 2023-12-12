c3fe912ec97c9e19960b7b131f27f08372d1ded1
Ali Alafandy <107328467+AliAlafandy@users.noreply.github.com>
2023-12-12 10:25:07 +0200

    Add files via upload

--- build-crash-handler-macos.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-cd crashHandler
-echo Building crash dialog...
-haxelib run lime build macos
-copy build\openfl\macos\bin\SBCrashHandler.exe ..\export\release\macos\bin\SBCrashHandler.exe
-cd ..
-
-@echo on
