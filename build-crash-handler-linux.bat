0c801bb3a3618dc31fe653d7e33e081944a4e05a
Ali Alafandy <107328467+AliAlafandy@users.noreply.github.com>
2023-12-12 10:24:47 +0200

    Add files via upload

--- build-crash-handler-linux.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-cd crashHandler
-echo Building crash dialog...
-haxelib run lime build linux
-copy build\openfl\linux\bin\SBCrashHandler.exe ..\export\release\linux\bin\SBCrashHandler.exe
-cd ..
-
-@echo on
