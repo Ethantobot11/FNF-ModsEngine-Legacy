dd94a32dcd4528f64e2d01f06d50b9ab64746349
Ali Alafandy <107328467+AliAlafandy@users.noreply.github.com>
2024-01-23 23:23:38 +0200

    Add files via upload

--- source/states/TitleState.hx
+++ source/states/TitleState.hx
@@ -549,9 +549,11 @@ class TitleState extends MusicBeatState
 		}
 	}
 
-	private function createHint(X:Float, Y:Float, Graphic:String, Color:Int = 0xFFFFFF): {
+	#if android
+	private function createHint(X:Float, Y:Float, Graphic:String, Color:Int = 0xFFFFFFFF):
 	hint.loadGraphic(FlxGraphic.fromFrame(FlxAtlasFrames.fromSparrow(Assets.getBitmapData('assets/android/keyboard.png');
-	}
+	#end
+	
 	private var sickBeats:Int = 0; //Basically curBeat but won't be skipped if you hold the tab or resize the screen
 	public static var closedState:Bool = false;
 	override function beatHit()
