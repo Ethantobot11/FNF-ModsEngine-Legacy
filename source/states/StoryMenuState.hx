d81b399c97b2f6ac76751ef6f6ea3a6b87f3fca2
Ali Alafandy <107328467+AliAlafandy@users.noreply.github.com>
2024-01-23 23:31:36 +0200

    Add files via upload

--- source/states/StoryMenuState.hx
+++ source/states/StoryMenuState.hx
@@ -179,9 +179,9 @@ class StoryMenuState extends MusicBeatState
 		changeWeek();
 		changeDifficulty();
 
-        #if android
-        addVirtualPad(LEFT_FULL, A_B_X_Y);
-        #end
+        	#if android
+        	addVirtualPad(LEFT_FULL, A_B_X_Y);
+        	#end
 
 		super.create();
 	}
