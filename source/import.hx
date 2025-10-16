5d23fb1257604012f32cac515d19224220ceba30
Ethantobot <156458071+Ethantobot11@users.noreply.github.com>
2025-10-16 21:34:14 +0200

    Update import.hx

--- source/import.hx
+++ source/import.hx
@@ -25,6 +25,30 @@ import backend.Mods;
 import objects.Alphabet;
 import objects.BGSprite;
 
+//Mobile Controls
+import mobile.objects.MobileControls;
+import mobile.objects.IMobileControls;
+import mobile.objects.Hitbox;
+import mobile.objects.TouchPad;
+import mobile.objects.TouchButton;
+import mobile.input.MobileInputID;
+import mobile.backend.MobileData;
+import mobile.input.MobileInputManager;
+import mobile.backend.TouchUtil;
+
+// Android
+#if android
+import android.content.Context as AndroidContext;
+import android.widget.Toast as AndroidToast;
+import android.os.Environment as AndroidEnvironment;
+import android.Permissions as AndroidPermissions;
+import android.Settings as AndroidSettings;
+import android.Tools as AndroidTools;
+import android.os.Build.VERSION as AndroidVersion;
+import android.os.Build.VERSION_CODES as AndroidVersionCode;
+import android.os.BatteryManager as AndroidBatteryManager;
+#end
+
 import states.PlayState;
 import states.LoadingState;
 
